6775c8459b2523af2deac385d6e4949aaff2a752
VictorSouzaa0 <victor.r.souzaa05@gmail.com>
2024-09-27 23:53:11 -0300

    27.09.24

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    codigoProduto = models.CharField(max_length=255)
+    tituloProduto = models.CharField(max_length=255)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    descricao = models.CharField(max_length=255)
+    imagemProduto = models.CharField(max_length=255)
+    categoriaProduto = models.CharField(max_length=255)
+    classProduto = models.DecimalField(max_digits=10, decimal_places=2)
+    exibirNome = models.BooleanField(default=True)
+ 
