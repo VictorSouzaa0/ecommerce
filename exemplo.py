51451c6f1ff0114642cacaec7976cd45ef308e56
Victor Renato de Souza <53796509886@>
2024-11-01 15:16:41 -0300

    1.11.2k24

--- exemplo.py
+++ exemplo.py
@@ -80,18 +80,3 @@ class Ecommerce:
         self.create(titulo, preco, descricao, imagemProduto, catProduto, classProduto, exibirNome)
 
 home = Ecommerce()
-
-# class Carro:
-#     def __init__(self,marca,modelo,ano):
-#             self.marca = marca
-#             self.modelo = modelo
-#             self.ano =ano
-
-#     def imprimir(self):
-#           return print(f"A marca do carro é {self.marca}, o ano {self.ano} e o modelo é {self.modelo}")
-
-# x = Carro("VW","UP","2016") 
-# x.imprimir
-
-# y = Carro("Toyota", "Corola","2022")
-# y.imprimir
