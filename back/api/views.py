1c1d804eaad0c0ac542bbe96ffe3c9a42bb0fd4e
Victor Renato de Souza <53796509886@>
2024-11-29 13:37:42 -0300

    13:40

--- back/api/views.py
+++ back/api/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from .models import Produto
-from .serializer import ProdutoSerializer
+from .serializers import ProdutoSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 @api_view(['GET', 'POST'])
 def listar_produtos(request):
@@ -18,3 +19,22 @@ def listar_produtos(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+        
+@api_view(['GET', 'PUT', 'DELETE'])
+def detalhar_produto(request, id):
+    produto = get_object_or_404(Produto, id=id)
+
+    if request.method == 'GET':
+        serializer = ProdutoSerializer(produto)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ProdutoSerializer(produto, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        produto.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
